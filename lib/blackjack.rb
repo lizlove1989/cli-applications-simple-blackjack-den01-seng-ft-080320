f99cdae7ca43cfd220dd817a9a7a6e373f6e9910
Liz Love <love.elizabeth1989@gmail.com>
2020-07-20 19:58:35 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -37,10 +37,10 @@ end
 def hit?(card_total)
   prompt_user
   user_input = get_user_input
-  if user_input == 'h'
-     card_total += deal_card
-   elsif user_input == 's'
+  if user_input == "s"
      card_total
+   elsif user_input == "h"
+     card_total += deal_card
    else 
      invalid_command
    end 

c9bfc079e08c865ac969804ee957ef9c8538b509
Liz Love <love.elizabeth1989@gmail.com>
2020-07-20 20:27:52 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -57,12 +57,12 @@ end
 
 def runner
   welcome
-  # card_total = initial_round
-  # hit?
-  # until card_total >= 21
-  #   card_total = hit?(card_total)
-  #   display_card_total(card_total)
-  # end
-  # end_game(card_total)
+  sleep 0.75
+  card_total = initial_round
+  until card_total > 21
+    card_total = hit?(card_total)
+    display_card_total(card_total)
+  end
+  end_game(card_total)
 end
 
